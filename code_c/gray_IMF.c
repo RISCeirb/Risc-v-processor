9fd880bb4b1f9b5b2009d8a8611ff6d91fe42c00
thibaultschwab43 <159802602+RISCeirb@users.noreply.github.com>
2024-08-30 12:28:46 -0300

    Add files via upload

---
+++ code_c/gray_IMF.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    FILE *fIn = fopen("input.bmp", "rb");
+    FILE *fOut = fopen("output.bmp", "wb");
+    if (!fIn || !fOut)
+    {
+        printf("File error.\n");
+        return 0;
+    }
+
+    unsigned char header[54];
+    fread(header, sizeof(unsigned char), 54, fIn);
+    fwrite(header, sizeof(unsigned char), 54, fOut);
+
+    int width = *(int*)&header[18];
+    int height = abs(*(int*)&header[22]);
+    int stride = (width * 3 + 3) & ~3;
+    int padding = stride - width * 3;
+
+    printf("width: %d (%d)\n", width, width * 3);
+    printf("height: %d\n", height);
+    printf("stride: %d\n", stride);
+    printf("padding: %d\n", padding);
+
+    unsigned char pixel[3];
+    for (int y = 0; y < height; ++y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            fread(pixel, 3, 1, fIn);
+            printf("R = %d G = %d B = %d\n", pixel[0], pixel[1], pixel[2]); // affiche les pixels de l'image
+            unsigned char gray = pixel[0] * 0.3 + pixel[1] * 0.58 + pixel[2] * 0.11;
+            memset(pixel, gray, sizeof(pixel));
+            fwrite(&pixel, 3, 1, fOut);
+        }
+        fread(pixel, padding, 1, fIn);
+        fwrite(pixel, padding, 1, fOut);
+    }
+    fclose(fOut);
+    fclose(fIn);
+    return 0;
+}
